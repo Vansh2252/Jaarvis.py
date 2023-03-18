dd9f4d96ac08e9d18d0193fe74f39420f544b74d
Vanshikaduggal <118618760+Vanshikaduggal@users.noreply.github.com>
2023-03-18 13:47:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,4 +27,21 @@ if hour>=0 and hour<12:
 
     speak("I am excalibur Sir. Please tell me how may I help you")       
 def takeCommand():
-    
+     r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
