16a5962d5ef2f63ffc71c49aaa27e1ea1f8af523
Vanshikaduggal <118618760+Vanshikaduggal@users.noreply.github.com>
2023-03-18 13:15:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import pyttsx3 
+import speech_recognition as sr 
+import datetime
+import wikipedia 
+import webbrowser
+import os
+
+print("press 0 for male voice assistant,press 1 for female voice assistant")
+n=(int)(input())
+engine = pyttsx3.init('sapi5')
 
