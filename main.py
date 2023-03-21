46cae58827d7dab03848e80e3b25dbb6652d90d3
Vansh2252 <117134449+Vansh2252@users.noreply.github.com>
2023-03-21 10:05:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,25 +52,7 @@ def takeCommand():
         query = takeCommand().lower()
 
         
-        if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
-               elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
-
-        elif 'open google' in query:
-            webbrowser.open("google.com")
-
-        elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")               
-
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f"Sir, the time is {strTime}")
+        
   
             
 
